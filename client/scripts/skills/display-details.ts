311d27a50c577f27496e9b1b8d5b924eab0f254e
Andy Swanepoel <andyp.swanepoel@gmail.com>
2023-04-30 11:31:16 -0400

    timeline scrollbar

--- client/scripts/skills/display-details.ts
+++ client/scripts/skills/display-details.ts
@@ -2,13 +2,12 @@ const showCard = (event: Event) => {
   const target = event.currentTarget as HTMLElement;
   const skill = target.getAttribute("data-skill");
 
-  document
-    .querySelectorAll("[data-skill-card]")
-    .forEach(card => card.classList.remove("grid__skill__card__visible"));
-
-  document
-    .querySelector(`[data-skill-card='${skill}']`)
-    ?.classList.add("grid__skill__card__visible");
+  document.querySelectorAll("[data-skill-card]").forEach(card => {
+    card.classList.remove("grid__skill__card__visible");
+    if (card.getAttribute("data-skill-card") === skill) {
+      card.classList.add("grid__skill__card__visible");
+    }
+  });
 };
 
 export default () => {
--- client/scripts/timeline/timeline.ts
+++ client/scripts/timeline/timeline.ts
@@ -2,16 +2,40 @@ const changeYear = (event: Event) => {
   const target = event.currentTarget as HTMLElement;
   const year = target.getAttribute("data-timeline-selector");
 
-  document
-    .querySelectorAll("[data-timeline-year]")
-    .forEach(card => card.classList.remove("timeline__experience__visible"));
+  document.querySelectorAll("[data-timeline-year]").forEach(exp => {
+    exp.classList.remove("timeline__experience__visible");
+    if (exp.getAttribute("data-timeline-year") === year) {
+      exp.classList.add("timeline__experience__visible");
+    }
+  });
 
-  document
-    .querySelectorAll(`[data-timeline-year='${year}']`)
-    .forEach(exp => exp.classList.add("timeline__experience__visible"));
+  document.querySelectorAll("[data-timeline-selector]").forEach(btn => {
+    btn.classList.remove("timeline__button__selected");
+    if (btn === target) {
+      btn.classList.add("timeline__button__selected");
+    }
+  });
+
+  scrollToSelectedYear();
+};
+
+const scrollToSelectedYear = () => {
+  const scrollWindow = document.getElementById(
+    "timeline__window"
+  ) as HTMLElement;
+  const selectedYear = scrollWindow?.querySelector(
+    ".timeline__button__selected"
+  ) as HTMLElement;
+
+  const selectedYearContainer = selectedYear.parentElement as HTMLElement;
+
+  const scrollTo =
+    selectedYear.clientWidth / 2 + selectedYearContainer.offsetLeft;
+  scrollWindow.style.transform = `translateX(calc(50% - ${scrollTo}px))`;
 };
 
 export default () => {
+  scrollToSelectedYear();
   document.querySelectorAll("[data-timeline-selector]").forEach(btn => {
     btn.addEventListener("click", changeYear);
   });
