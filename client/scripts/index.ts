894bd00ae87b80362677a3365ac0811ee2191ebc
Andy Swanepoel <andyp.swanepoel@gmail.com>
2023-09-30 12:04:58 -0400

    feat: update skills layout

--- client/scripts/index.ts
+++ client/scripts/index.ts
@@ -1,7 +1,5 @@
 import * as timelineScripts from "./timeline";
-import * as skillScripts from "./skills";
 import * as navigationScripts from "./navigation";
 
 Object.keys(navigationScripts).forEach(script => navigationScripts[script]());
-Object.keys(skillScripts).forEach(script => skillScripts[script]());
 Object.keys(timelineScripts).forEach(script => timelineScripts[script]());
--- client/scripts/skills/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as skillListener } from "./display-details";
