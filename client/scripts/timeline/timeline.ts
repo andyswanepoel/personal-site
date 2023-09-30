e0d29e6c9bc64f4f6ed6580ec23f194bede2788e
Andy Swanepoel <andyp.swanepoel@gmail.com>
2023-09-30 13:24:20 -0400

    feat: update timeline

--- client/scripts/timeline/timeline.ts
+++ client/scripts/timeline/timeline.ts
@@ -1,77 +1,24 @@
-const scrollToContent = (event: Event) => {
-  const target = event.currentTarget as HTMLElement;
-  const year = target.getAttribute("data-timeline-selector");
-
-  document
-    .querySelector(`[data-timeline-year='${year}']`)
-    ?.scrollIntoView({ behavior: "smooth" });
-};
-
-const changeTimelineYear = (year: string) => {
-  document
-    .querySelectorAll("[data-timeline-selector]")
-    .forEach(btn => btn.classList.remove("timeline__button__selected"));
-
-  document
-    .querySelector(`[data-timeline-selector='${year}']`)
-    ?.classList.add("timeline__button__selected");
-
-  scrollToSelectedYear();
-};
-
-let scrollToYearTimeout;
-
-const changeYearScroll = (entry: IntersectionObserverEntry) => {
-  if (scrollToYearTimeout) {
-    clearTimeout(scrollToYearTimeout);
-  }
-
+const addVisibleClass = (entry: IntersectionObserverEntry) => {
   if (entry.isIntersecting) {
-    console.log("entry: ", entry);
-    const year = entry.target.getAttribute("data-timeline-year");
-
-    scrollToYearTimeout = setTimeout(() => {
-      if (year) {
-        console.log("@@@@fitring");
-        changeTimelineYear(year);
-      }
-    }, 50);
+    entry.target.classList.add("timeline__year__container__visible");
+    observer.unobserve(entry.target);
   }
-};
-
-const scrollToSelectedYear = () => {
-  const scrollWindow = document.getElementById(
-    "timeline__window"
-  ) as HTMLElement;
-  const selectedYear = scrollWindow?.querySelector(
-    ".timeline__button__selected"
-  ) as HTMLElement;
-
-  const selectedYearContainer = selectedYear.parentElement as HTMLElement;
-
-  const scrollTo =
-    selectedYear.clientWidth / 2 + selectedYearContainer.offsetLeft;
-  scrollWindow.style.transform = `translateX(calc(50% - ${scrollTo}px))`;
 };
 
 const timelineObserverOptions = {
   root: document.getElementById("timeline__experience__container"),
-  rootMargin: "0px",
-  threshold: 0.9
+  rootMargin: "-100px",
+  threshold: 0
 };
 
 const callback = (entries: IntersectionObserverEntry[]) => {
-  entries.forEach(changeYearScroll);
+  entries.forEach(addVisibleClass);
 };
 
 let observer = new IntersectionObserver(callback, timelineObserverOptions);
 
 export default () => {
-  document.querySelectorAll("[data-timeline-selector]").forEach(btn => {
-    btn.addEventListener("click", scrollToContent);
+  document.querySelectorAll("[data-timeline-year]").forEach(year => {
+    observer.observe(year);
   });
-
-  document
-    .querySelectorAll(".timeline__experience__wrapper")
-    .forEach(w => observer.observe(w));
 };
